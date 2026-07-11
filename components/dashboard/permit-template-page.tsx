fc0825c454df75b66df3f73dd12671643aa9d7a6
FyslAhmd <info.faysal.32@gmail.com>
2026-07-11 09:52:52 +0600

    feat: replace re-export of PermitTemplatePage with new client-side component scaffold

--- components/dashboard/permit-template-page.tsx
+++ components/dashboard/permit-template-page.tsx
@@ -1,1 +1,11 @@
-export { PermitTemplatePage } from "../permit-template/permit-template-page";
+"use client";
+
+import React from "react";
+
+export function PermitTemplatePage() {
+  return (
+    <div className="flex flex-col gap-8 p-6 text-brand-primary">
+      {/* Empty Permit Template Route - Ready for New Design */}
+    </div>
+  );
+}
